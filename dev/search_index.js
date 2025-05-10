bb2cd15564b06e1f09a835bbbcfab67ac9e1d69c
Documenter.jl <documenter@juliadocs.github.io>
2025-05-10 00:31:48 +0000

    build based on ee11d95

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Mallows","page":"Home","title":"Mallows","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Mallows.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
